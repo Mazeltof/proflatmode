d8efe4443fad8e677412a591749a47a7150ce994
Mazeltof <Mazeltof@live.fr>
2024-11-25 19:34:07 +0100

    Add files via upload

---
+++ styles/proflat/template/mode.js
@@ -0,0 +1,23 @@
+// Switch light/dark theme
+$(function () {
+	let $switchBtn = $("#switch-theme"),
+		osDarkscheme = window.matchMedia("(prefers-color-scheme: dark)"),
+		$html = $("html"),
+		dataAttr = "data-theme",
+		light = "light",
+		dark = "dark";
+
+	$switchBtn.click(function (e) {
+		let theme = "";
+
+		if (osDarkscheme.matches) {
+			$html.attr(dataAttr, $html.attr(dataAttr) === light ? dark : light);
+			theme = $html.attr(dataAttr) === light ? light : dark;
+		} else {
+			$html.attr(dataAttr, $html.attr(dataAttr) === dark ? light : dark);
+			theme = $html.attr(dataAttr) === dark ? dark : light;
+		}
+		localStorage.setItem("proflatmode", theme);
+		e.preventDefault();
+	});
+});
